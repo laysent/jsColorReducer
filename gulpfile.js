8ce359df38491f72e7b9dcec94bed1a6060448cd
Zhou YU <laysent@gmail.com>
2016-01-29 23:48:16 +0800

    minify javascript code

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('default', function () {
+  return gulp.src('bundle.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('.'));
+  }
+);
